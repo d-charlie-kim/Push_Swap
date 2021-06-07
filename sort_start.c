7a5145bbb14a529534fdac48320e9f72e3d7732f
charlie com <dokkim@student.42seoul.kr>
2021-06-08 01:42:57 +0900

    logic fixing

--- sort_start.c
+++ sort_start.c
@@ -17,10 +17,12 @@ void	ft_sort(t_stack **stack_a, t_stack **stack_b, int count)
 	int		first;
 	int		second;
 	int		temp;
-	int		next_chunk;
+	int		first_chunk;
+	int		second_chunk;
 	
 	temp = count;
-	next_chunk = 0;
+	first_chunk = 0;
+	second_chunk = 0;
 	if (count > 3)
 	{
 		first = ft_find_pivot(stack_a, stack_b, count, 1);
@@ -35,10 +37,13 @@ void	ft_sort(t_stack **stack_a, t_stack **stack_b, int count)
 			if (((*stack_a)->number) >= second)
 			{
 				rotate_a(stack_a);
-				next_chunk++;
+				first_chunk++;
 			}
 			else if (second > ((*stack_a)->number) && ((*stack_a)->number) >= first)
+			{
 				push_b(stack_a, stack_b);
+				second_chunk++;
+			}
 			else if (first > ((*stack_a)->number))
 			{
 				push_b(stack_a, stack_b);
@@ -46,14 +51,14 @@ void	ft_sort(t_stack **stack_a, t_stack **stack_b, int count)
 			}
 			temp--;
 		}
-		temp = next_chunk + 1;
+		temp = first_chunk + 1;
 		while (--temp > 0)
 			reverse_rotate_a(stack_a);
-		ft_sort(stack_a, stack_b, next_chunk);
+		ft_sort(stack_a, stack_b, first_chunk);
 	}
 	if (count < 4)
 		two_or_three_nor(stack_a, stack_b, count);
-	temp = count - next_chunk;
+	temp = second_chunk;
 	if (count > 3 && temp > 0)
 	{
 		while (temp > 0)
@@ -61,7 +66,17 @@ void	ft_sort(t_stack **stack_a, t_stack **stack_b, int count)
 			push_a(stack_a, stack_b);
 			temp--;
 		}
-		ft_sort(stack_a, stack_b, count - next_chunk);
+		ft_sort(stack_a, stack_b, second_chunk);
+	}
+	temp = count - first_chunk - second_chunk;
+	if (count > 3 && temp > 0)
+	{
+		while (temp > 0)
+		{
+			push_a(stack_a, stack_b);
+			temp--;
+		}
+		ft_sort(stack_a, stack_b, count - first_chunk - second_chunk);
 	}
 }
 
@@ -85,9 +100,20 @@ void	two_or_three(t_stack **stack_a, t_stack **stack_b, int count)
 
 void	two_or_three_nor(t_stack **stack_a, t_stack **stack_b, int count)
 {
-	int	i;
+	int		i;
+	int		num;
+	t_stack	*temp;
 
 	i = 0;
+	num = 3;
+	temp = *stack_a;
+	while (num > 0 && temp != NULL)
+	{
+		temp = temp->next;
+		num--;
+	}
+	if (temp == NULL)
+		return(two_or_three(stack_a, stack_b, count));
 	if (count == 2)
 		two_arg(stack_a, stack_b);
 	else if (count == 3)
